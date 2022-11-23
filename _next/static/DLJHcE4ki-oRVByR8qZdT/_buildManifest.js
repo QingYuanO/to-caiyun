c50c48894618ef506e2b2d9ba9e97c5342260edb
QingYuanO <QingYuanO@users.noreply.github.com>
2022-11-23 15:11:21 +0000

    deploy: 16c5f2e0f0ebeded94a3c07843855f08e758b2df

---
+++ _next/static/DLJHcE4ki-oRVByR8qZdT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/37f91b14b999a1f7.css","static/chunks/pages/index-14ea690ff25f7225.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
